76221b89c80b36ddad0b7df7a86e690c73609698
daniel.alaniz <daniel.alaniz@zfinal.com>
2019-11-21 12:27:01 -0300

    Intento hacer una tabla basica JQuery

--- ejemplo/encuestas.php
+++ ejemplo/encuestas.php
@@ -1,131 +1,56 @@
+<?php
+include_once("../inc/check_session.php");
+$msg = "";
+?>
 <!doctype html>
 <html lang="en" dir="ltr">
   <head>
     <title>jQuery SCRUD system</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=1000, initial-scale=1">
+
+    <!--meta name="viewport" content="width=1000, initial-scale=1"-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../css/common.css" type="text/css" />
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" type="text/css" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Oxygen:400,700">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="encuestas.css">
+	
+	
+
+    <!--script charset="utf-8" src="https://code.jquery.com/jquery-3.3.1.js"></script-->
     <script charset="utf-8" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script charset="utf-8" src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
+    <!-- script charset="utf-8" src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script -->
+    <script charset="utf-8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    
     <script charset="utf-8" src="//cdn.jsdelivr.net/jquery.validation/1.13.1/jquery.validate.min.js"></script>
     <script charset="utf-8" src="encuestas.js"></script>
   </head>
   <body>
 
-    <div id="page_container">
-		<a target="_blank" href="https://www.sitepoint.com/creating-a-scrud-system-using-jquery-json-and-datatables/">Referencia</a>
-      <h1>Ejemplo JQuery - Compa&ntilde;ias IT </h1>
-	  
-      <button type="button" class="button" id="add_company">Add company</button>
+	<?php include_once("menu.php"); ?>
+<div class="workarea_v">  
 
-      <table class="datatable" id="table_companies">
+      <table class="display" id="tabla_encuestas">
         <thead>
           <tr>
-            <th>Rank</th>
-            <th>Company name</th>
-            <th>Industries</th>
-            <th>Revenue</th>
-            <th>Year</th>
-            <th>Employees</th>
-            <th>Market cap</th>
-            <th>Headquarters</th>
-            <th>Functions</th>
+            <th>#Id</th>
+            <th>Nombre</th>
+            <th>Descripci&oacute;n</th>
           </tr>
         </thead>
         <tbody>
-        </tbody>
+            <tr>
+                <td>1</td>
+                <td>Encuesta satisfaccion</td>
+                <td>Esta encuesta consulta / evalua............</td>
+            </tr>
+		</tbody>
       </table>
 
-    </div>
-
-    <div class="lightbox_bg"></div>
-
-    <div class="lightbox_container">
-      <div class="lightbox_close"></div>
-      <div class="lightbox_content">
-        
-        <h2>Add company</h2>
-        <form class="form add" id="form_company" data-id="" novalidate>
-          <div class="input_container">
-            <label for="rank">Rank: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="number" step="1" min="0" class="text" name="rank" id="rank" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="company_name">Company name: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="text" class="text" name="company_name" id="company_name" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="industries">Industries: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="text" class="text" name="industries" id="industries" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="revenue">Revenue: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="number" step="1" min="0" class="text" name="revenue" id="revenue" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="fiscal_year">Fiscal year: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="number" min="0" class="text" name="fiscal_year" id="fiscal_year" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="employees">Employees: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="number" min="0" class="text" name="employees" id="employees" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="market_cap">Market cap: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="number" step="1" min="0" class="text" name="market_cap" id="market_cap" value="" required>
-            </div>
-          </div>
-          <div class="input_container">
-            <label for="headquarters">Headquarters: <span class="required">*</span></label>
-            <div class="field_container">
-              <input type="text" class="text" name="headquarters" id="headquarters" value="" required>
-            </div>
-          </div>
-          <div class="button_container">
-            <button type="submit">Add company</button>
-          </div>
-        </form>
-        
-      </div>
-    </div>
-
-    <noscript id="noscript_container">
-      <div id="noscript" class="error">
-        <p>JavaScript support is needed to use this page.</p>
-      </div>
-    </noscript>
-
-    <div id="message_container">
-      <div id="message" class="success">
-        <p>This is a success message.</p>
-      </div>
-    </div>
-
-    <div id="loading_container">
-      <div id="loading_container2">
-        <div id="loading_container3">
-          <div id="loading_container4">
-            Loading, please wait...
-          </div>
-        </div>
-      </div>
-    </div>
-
+</div>
+<div class="mensajes">
+	<label id="texto_msg"><?php echo($msg); ?></label>
+</div>
   </body>
 </html>
