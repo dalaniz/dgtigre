840f3a9854e46e792a43405ef78d58493afe4dde
Tobias Lajud <58052589+tobiaslajud@users.noreply.github.com>
2019-11-28 20:35:56 -0300

    Add files via upload
    cambio el estilo

--- Tobias/registro.php
+++ Tobias/registro.php
@@ -13,6 +13,8 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : "";
 <link rel="stylesheet" href="../css/common.css" type="text/css" />
 
 <style>
+	@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
 	.cuadro-login {
 		display: flex;
 		flex-direction: column;
@@ -21,14 +23,82 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : "";
 		height: 250px;
 		width: 250px;
 		background-color: black;
-		color: DodgerBlue; 
-		font-size: 20px; 
+		color: tomato; 
+		font-size: 18px; 
 		font-weight: bold;
-		/*border-radius: 25px;*/
+		border-radius: 25px;
+		font-family: 'roboto', serif;
+		padding-bottom: 25px;
+	}
+
+	.cuadro-login label {
+	
 	}
 	.opcion-login:hover {
 		background-color: #d8d0d0;		
 	}
+	body {
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    color: #fff;
+    background: linear-gradient(-50deg, lightseagreen, lightskyblue, crimson);
+    background-size: 400% 400%;
+    animation: gradientBG 15s ease infinite;
+}
+
+@keyframes gradientBG {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
+.container {
+    width: 100%;
+    position: absolute;
+    top: 35%;
+    text-align: center;
+}
+
+h1 {
+    font-weight: 300;
+}
+
+h3 {
+    color: #eee;
+    font-weight: 100;
+}
+
+h5 {
+    color:#eee;
+    font-weight:300;
+}
+
+a,
+a:hover {
+    text-decoration: none;
+    color: #ddd;
+}
+
+.logo {
+	width: 175px;
+	height: 100px;	
+	position: absolute;
+	top: 400px;
+	
+}
+
+body {
+	style="background-color: #13eb96; color: black"
+}
+
+
 </style>
 
 <script>
@@ -51,13 +121,13 @@ function validarPwd() {
 
 <body>
 
-<div class="titulo_principal"> <label style="background-color: black; color: DodgerBlue; font-size: 35px; font-weight: bold;">DIGIT@LERS - TIGRE</label>
+<div class="titulo_principal"> <label style="background-color: black; color: tomato; font-size: 35px; font-weight: bold;">DIGIT@LERS - TIGRE</label>
 </div>
 
 <div class="workarea">
 	<form action="insert_user.php" method="POST" id="form_registro" >
 	<div class="cuadro-login">
-		<label style="height: 40px; background-color: black; color: DodgerBlue;">INGRESO</label>
+		<label style="height: 40px; background-color: black; color: tomato;">INGRESO</label>
 		
 		<label>Usuario</label>
 		<input type="text" id="user" name="user" />
