11386e71a850a35afe752d663cfa50af06769ce1
Hanasauria <solm.s@hotmail.com>
2019-11-28 18:49:22 -0300

    Agrego login, menu, registro.

--- Sakamoto/login.php
+++ Sakamoto/login.php
@@ -9,21 +9,40 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : "";
 <link rel="stylesheet" href="../css/common.css" type="text/css" />
 
 <style>
+
+    .titulo_principal { 
+	display:flex;
+	justify-content: center;
+	align-items: center;
+	height: 100 px;
+	background-color: #173c68;
+	font-family: "verdana";
+	font-size: 30px;
+	color: #002b80 ;
+	
+	}
+
 	.cuadro-login {
 		display: flex;
+		border-radius: 25px;
 		flex-direction: column;
 		justify-content: center; 
 		align-items: center;
-		height: 250px;
-		width: 250px;
-		background-color: black;
-		color: DodgerBlue; 
+		height: 270px;
+		width: 240px;
+		background-color:  #f3ffbd;
+		color: #002b80; 
 		font-size: 20px; 
 		font-weight: bold;
 		/*border-radius: 25px;*/
 	}
 	.opcion-login:hover {
 		background-color: #d8d0d0;		
+	}
+	body 
+	{ background: rgb(112,193,179);
+      background: linear-gradient(90deg, rgba(112,193,179,1) 0%, rgba(55,121,139,1) 50%, rgba(35,110,170,1) 100%);;
+	  font-family: Verdana, Geneva, sans-serif;
 	}
 </style>
 
@@ -32,20 +51,20 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : "";
 
 <body>
 
-<div class="titulo_principal"> <label style="background-color: black; color: DodgerBlue; font-size: 35px; font-weight: bold;">DIGIT@LERS - TIGRE</label>
+<div class="titulo_principal"> <label style="background-color: #173c68; color: white; font-size: 30px; font-weight: bold;">DIGIT@LERS - TIGRE</label>
 </div>
 
 <div class="workarea">
 	<form action="check_user.php" method="POST" >
 	<div class="cuadro-login">
-		<label style="height: 40px; background-color: black; color: DodgerBlue;">INGRESO</label>
+		<label style="height: 40px; background-color: #f3ffbd; color: #002b80;">INGRESO</label>
 		
 		<label>Usuario</label>
 		<input type="text" id="user" name="user" />
 		<label>Contrase&ntilde;a</label>
 		<input type="password" id="pwd" name="pwd"/>
 		<input type="submit" value="ACEPTAR" id="loginButton" />
-		<a style="color: white; font-size:15px;" href="registro.php">Registrarse</a>
+		<a style="color: #ff1654; font-size:15px;" href="registro.php">Registrarse</a>
 	</div>
 	</form>
 </div>
--- Sakamoto/menu.php
+++ Sakamoto/menu.php
@@ -1,4 +1,12 @@
-<div class="menu"> 
+<style>
+
+	body 
+	{ background: rgb(112,193,179);
+      background: linear-gradient(90deg, rgba(112,193,179,1) 0%, rgba(55,121,139,1) 50%, rgba(35,110,170,1) 100%);;
+	  font-family: Verdana, Geneva, sans-serif;
+	}
+</style>
+<div class="menu"> <label style="background-color: #173c68;">
 	 <a href="http://www.digitalers.com.ar/" target="_blank" style="margin-left:10px; margin-right: 10px;"><img src="../img/logo_telecom_digitalers.png" alt="DIGIT@LERS" height="32" style> </a>
 	 <div class="menu_option" onclick="window.location.href = 'encuestas.php';">Encuestas</div>
 	 <div class="menu_option" onclick="window.location.href = 'noimplementado.php';">Resultados</div>
