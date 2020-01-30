a6bbfe7802fe3f7210606b587a0077a49a416137
Ivan <ivan@mmm.com>
2020-01-30 18:38:28 -0300

    modificacion

--- ivansalto/bienvenido.php
+++ ivansalto/bienvenido.php
@@ -7,7 +7,7 @@ $msg = "";
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../css/common.css" type="text/css" />
+<link rel="stylesheet" href="../css/salto.css?v=<?php echo(rand()); ?>" type="text/css" />
 
 </head>
 
