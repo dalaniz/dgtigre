42ba67ecee064d49358ab3c0e95a9de77ea0cd09
Ivan <ivan@mmm.com>
2020-03-24 19:17:11 -0300

    modificacion

--- ivansalto/registro.php
+++ ivansalto/registro.php
@@ -10,7 +10,7 @@ $msg = isset($_GET['msg']) ? $_GET['msg'] : "";
 <head>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../css/common.css" type="text/css" />
+<link rel="stylesheet" href="../css/salto.css?v=<?php echo(rand()); ?>" type="text/css" />
 
 <style>
 	.cuadro-login {
